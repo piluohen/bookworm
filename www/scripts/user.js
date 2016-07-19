b9584d22d3fcb8baa19bfdcd7e616e7e1f189f0c
piluohen <1012106967@qq.com>
2016-07-19 08:34:30 +0800

    完成购物车、书架、订单、帮助静态页面并修改整体页面色调为浅黄色

--- www/scripts/user.js
+++ www/scripts/user.js
@@ -4,25 +4,5 @@ m.controller('UserCtrl', ['$scope', '$http', '$window', function ($scope, $http,
         $scope.data = res
     })
     var username = $.cookie('username')
-    if (username) {
-        $('#my>i').text('花径不曾缘客扫，蓬门今始为君开')
-        $('#sign').text(username).click(function () {
-            $window.location.href = 'user.html'
-        })
-        $('#register').text('退出登录').click(function () {
-            $http.get('/user/signout', null).success(function (res) {
-                if (res.code == 'success') {
-                    $window.location.href = 'index.html'
-                }
-            })
-        })
-    }
-    else {
-        $('#sign').text('请登录').click(function () {
-            $window.location.href = 'sign.html'
-        })
-        $('#register').text('免费注册').click(function () {
-            $window.location.href = 'register.html'
-        })
-    }
+    $('#name').text(username)
 }])
