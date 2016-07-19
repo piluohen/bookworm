8f96b9d8054a44b1eda2e1ee0a9f402dfd29dec7
piluohen <1012106967@qq.com>
2016-07-19 14:21:29 +0800

    设置非登录状态，购物车、订单、书架、用户中心点击进入登录页面

--- www/scripts/header.js
+++ www/scripts/header.js
@@ -3,23 +3,38 @@ angular.module('myApp').controller('Header', ['$scope', '$window', '$http',
         var username = $.cookie('username')
         if (username) {
             $('#my>i').text('花径不曾缘客扫，蓬门今始为君开')
-            $('#sign').text(username).click(function () {
+            $('#my>span#sign').text(username).click(function () {
                 $window.location.href = 'user.html'
             })
-            $('#register').text('退出登录').click(function () {
+            $('#my>span#register').text('退出登录').click(function () {
                 $http.get('/user/signout', null).success(function (res) {
                     if (res.code == 'success') {
                         $window.location.href = 'index.html'
                     }
                 })
             })
+            $('#my>span#shopbar').click(function(){
+                $window.location.href = 'shop.html'
+            })
+            $('#my>span#bookracks').click(function(){
+                $window.location.href = 'bookrack.html'
+            })
+            $('#my>span#indents').click(function(){
+                $window.location.href = 'indent.html'
+            })
+            $('#my>span#users').click(function(){
+                $window.location.href = 'user.html'
+            })
         }
         else {
-            $('#sign').text('请登录').click(function () {
+            $('#my>span#sign').text('请登录').click(function () {
                 $window.location.href = 'sign.html'
             })
-            $('#register').text('免费注册').click(function () {
+            $('#my>span#register').text('免费注册').click(function () {
                 $window.location.href = 'register.html'
+            })
+            $('#my>span#shopbar,#my>span#bookracks,#my>span#indents,#my>span#users').click(function(){
+                $window.location.href = 'sign.html'
             })
         }
     }
